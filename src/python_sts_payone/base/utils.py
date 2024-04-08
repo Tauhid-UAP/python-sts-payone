9d42fe1445adff392c284b7c5a6a292f3f255b6e
Kamruzzaman Tauhid <tauhid@Kamruzzamans-MacBook-Pro.local>
2024-04-08 16:17:05 +0600

    Live SR URLs

--- src/python_sts_payone/base/utils.py
+++ src/python_sts_payone/base/utils.py
@@ -1,10 +1,10 @@
 SR_URL_TEST: str = 'https://smartroute-test.payone.io/SmartRoutePaymentWeb/SRPayMsgHandler'
-SR_URL_LIVE: str = 'https://smartroute-test.payone.io/SmartRoutePaymentWeb/SRPayMsgHandler'
+SR_URL_LIVE: str = 'https://smartroute.payone.io/SmartRoutePaymentWeb/SRPayMsgHandler'
 
 
 """SR URL for Inquiry, Refund, Recurring"""
 SR_URL_SUPPORT_TEST: str = 'https://smartroute-test.payone.io/SmartRoutePaymentWeb/SRMsgHandler'
-SR_URL_SUPPORT_LIVE: str = 'https://smartroute-test.payone.io/SmartRoutePaymentWeb/SRMsgHandler'
+SR_URL_SUPPORT_LIVE: str = 'https://smartroute.payone.io/SmartRoutePaymentWeb/SRMsgHandler'
 
 REDIRECT_MESSAGE_ID: str = '1'
 REDIRECT_VERIFY_MESSAGE_ID: str = '14'
