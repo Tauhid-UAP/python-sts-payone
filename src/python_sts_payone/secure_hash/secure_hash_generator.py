9af33221f40303d367f94f42f49318c81a283a44
Kamruzzaman Tauhid <tauhid@Kamruzzamans-MacBook-Pro.local>
2024-07-31 11:22:09 +0600

    Skip null values in secure hash input string | Classmethod: generate_secure_hash_from_joined_params

--- src/python_sts_payone/secure_hash/secure_hash_generator.py
+++ src/python_sts_payone/secure_hash/secure_hash_generator.py
@@ -1,7 +1,6 @@
 from hashlib import sha256
 from collections import OrderedDict
 
-
 class SecureHashGenerator:
     def __init__(self, auth_token: str, params: dict):
         self.auth_token: str = auth_token
@@ -13,6 +12,13 @@ class SecureHashGenerator:
     def make_secure_hash_input_string(self) -> str:
         joined_param_values: str = ''
         for key, value in self.params.items():
+            if value is None:
+                """
+                Skip None values
+                Since STS PayOne sends the value as 'null', the consumer of this class must ensure that it is deserialized to None before passing the params here
+                """
+                continue
+
             value_to_concat: str = str(value)
             if key in ('CardHolderName', 'Response.CardHolderName', ):
                 joined_param_values += value_to_concat
@@ -25,10 +31,13 @@ class SecureHashGenerator:
 
         return self.auth_token + joined_param_values
     
+    @classmethod
+    def generate_secure_hash_from_joined_params(cls, joined_params: str) -> str:
+        return sha256(joined_params.encode('utf-8')).hexdigest()
+
     def generate_secure_hash(self) -> str:
         input_string: str = self.make_secure_hash_input_string()
-        secure_hash: str = sha256(input_string.encode('utf-8')).hexdigest()
-        return secure_hash
+        return self.generate_secure_hash_from_joined_params(input_string)
     
 def get_secure_hash(auth_token: str, params: dict) -> str:
     return SecureHashGenerator(auth_token, params).generate_secure_hash()
